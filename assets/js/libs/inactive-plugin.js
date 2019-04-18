ceee94c78419a5ad9f96d4c3951b305a7ae48ffc
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-18 00:12:18 -0300

    Collabrotext Commit

--- assets/js/libs/inactive-plugin.js
+++ assets/js/libs/inactive-plugin.js
@@ -3,26 +3,26 @@ $(document).ready(function() {
     const imageDir = window.location.pathname + "wp-content/themes/gabaritando/assets/images/background/";
     let click = 0;
 
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-};
+// function isMobileDevice() {
+//     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+// };
 
-    $('.control').click(function() {
-        let currentSlide = $('.swiper-slide-active').data('swiper-slide-index');
-        console.log(currentSlide);
+//     $('.control').click(function() {
+//         let currentSlide = $('.swiper-slide-active').data('swiper-slide-index');
+//         console.log(currentSlide);
 
-        if(isMobileDevice() != true){
-            console.log(0);
-            changeBg(currentSlide);
-        }
-    });
+//         if(isMobileDevice() != true){
+//             console.log(0);
+//             changeBg(currentSlide);
+//         }
+//     });
 
-    function changeBg(i) {
-        $('.main-header').css({
-            'background-image': 'url(' + imageDir + images[i] + ')',
-        });
-        console.log('entrei na função');
-    }
+//     function changeBg(i) {
+//         $('.main-header').css({
+//             'background-image': 'url(' + imageDir + images[i] + ')',
+//         });
+//         console.log('entrei na função');
+//     }
 
     $(window).scroll(function(){
         if($(this).scrollTop()>170){
