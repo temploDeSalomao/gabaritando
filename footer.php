a5ccfe2a34b6dcf2ba4d9ec355f20ec33dd654bd
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-18 00:35:36 -0300

    Collabrotext Commit

--- footer.php
+++ footer.php
@@ -116,7 +116,7 @@
 									<label for="" class="fields_name">
 										<input type="text" placeholder="Informe seu nome">
 									</label>
-									<label for="" class="field_tel">
+									<label for="" class="fields_tel">
 										<input type="text" placeholder="Informe seu telefone" />
 									</label>
 									<input type="submit" value="ME LIGUE DEPOIS!">
