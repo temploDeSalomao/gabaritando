353296da7b8a73f313d5038e8b69c56617acd7f8
Vitor de Lima <vitorblsantos@gmail.com>
2019-04-18 00:40:33 -0300

    Collabrotext Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,61 @@
 $(window).ready(function() {
-        var swiper_banner,
-            swiper_panel,
-            swiper_news,
-            swiper_plans;
+    var swiper_banner,
+        swiper_panel,
+        swiper_news,
+        swiper_plans;
 
-        swiper_banner = new Swiper('.swiper_banner', {
-            // Optional parameters
-            direction: 'horizontal',
-            loop: true,
-            navigation: {
-                nextEl: '.control.left.banner',
-                prevEl: '.control.right.banner',
-            },
-			sliderDrag:false
-        });
+    swiper_banner = new Swiper('.swiper_banner', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        navigation: {
+            nextEl: '.control.left.banner',
+            prevEl: '.control.right.banner',
+        },
+        sliderDrag: false
     });
 
-swiper_panel = new Swiper('.swiper-panel', {
-	 pagination: {
-        el: '.swiper-pagination-panel',
-      }
-});
 
-swiper_reviews = new Swiper('.swiper-review',{
-	 pagination: {
-        el: '.swiper-pagination-review',
-      }
-});
+    swiper_panel = new Swiper('.swiper-panel', {
+        pagination: {
+            el: '.swiper-pagination-panel',
+        }
+    });
 
-swiper_plan = new Swiper('.swiper-container-ticket',{
-		 pagination: {
-        el: '.swiper-pagination-ticket',
-      }
-})
+    swiper_reviews = new Swiper('.swiper-review', {
+        pagination: {
+            el: '.swiper-pagination-review',
+        }
+    });
 
-swiper_news = new Swiper('.swiper-news',{
-		 pagination: {
-        el: '.swiper-pagination-news',
-      }
-})
+    swiper_plan = new Swiper('.swiper-container-ticket', {
+        pagination: {
+            el: '.swiper-pagination-ticket',
+        }
+    })
+
+    swiper_news = new Swiper('.swiper-news', {
+        pagination: {
+            el: '.swiper-pagination-news',
+        }
+    })
 
 
 
 
-    $('.burger').click(function(){
+    $('.burger').click(function() {
         $('.master-content-menu-mobile').toggleClass('mobile-open');
 
-        if($('.master-content-menu-mobile').hasClass('mobile-open')){
+        if ($('.master-content-menu-mobile').hasClass('mobile-open')) {
             $('html,body').addClass('lock-overlay');
             $('.overlay').addClass('active');
 
             $('.burger').addClass('transform');
-        }
-        else{
+        } else {
             $('html,body').removeClass('lock-overlay');
             $('.burger').removeClass('transform');
             $('.overlay').removeClass('active');
         }
     });
+
+});
